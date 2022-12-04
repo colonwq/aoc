6edec88cc9e641559279eedadfe71035c9b26944
Keith Schincke <keith.schincke@gmail.com>
2022-12-04 12:43:49 -0600

    2020 day02-2

--- 2020/day02/code-2.py
+++ 2020/day02/code-2.py
@@ -7,14 +7,10 @@ Copy this directory to the new day and part
 
 import sys
 
-def test_password(first_char, second_char, req_char, password):
-    #print("min: %d max: %d req:%s password: %s" %(first_char, second_char, req_char, password) )
+def test_password(first_pos, second_pos, req_char, password):
+    #print("first pos: %d second pos: %d req:%s password: %s" %(first_pos, second_pos, req_char, password) )
     found = 0
-    if password[first_char] == req_char and password[second_char] == req_char:
-        #print("Valid password: %s" % (password) )
-        return True
-    else:
-        return False
+    return (password[first_pos-1] == req_char) != (password[second_pos-1] == req_char)
 
 def main():
     """Script entry point"""
@@ -34,7 +30,9 @@ def main():
         #print("minmax: %s req:%s password: %s" %(minmax, req_char, password) )
         min_char, max_char = minmax.split("-")
         #print("min:%s max: %s req:%s password: %s" %(min_char, max_char, req_char, password) )
+        #test_password(int(min_char), int(max_char), req_char, password)
         if test_password(int(min_char), int(max_char), req_char, password):
+            #print("Good: %s" %  ( line ) )
             good_passwords+=1
 
     print("Good Passwords: ", good_passwords )
