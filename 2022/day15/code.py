9b6a3a80a482fd278d708bc5de0f34c941cc0fa4
Keith Schincke <keith.schincke@gmail.com>
2022-12-22 09:54:58 -0600

    Day 15 part 1
    I started but did not finish part 2. I need to work some more
    wrinkles into the head. I left a helpful note for my future self

---
+++ 2022/day15/code.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python3
+'''
+Solution for Day00 part 1
+This is a generic file for all days.
+Copy this directory to the new day and part
+'''
+
+import sys
+
+def strip_lines(lines):
+    '''strip down the input lines to basic 
+    values SX,SY,BX,BY'''
+
+    new_lines = []
+
+    for line in lines:
+        #print("Line: %s" %(line) )
+        line = line.replace("Sensor at x=","")
+        line = line.replace(" y=","")
+        line = line.replace(": closest beacon is at ","")
+        line = line.replace("x=",",")
+        new_lines.append(line)
+
+    return new_lines
+
+def build_points_distance(lines):
+    '''take a pair of points
+    builds a list of first point and positive distance
+    to the second
+
+    example 2,18 -> -2,15 returns 2,18,7
+    '''
+    points_distance = []
+
+    for line in lines:
+        pd = []
+        points = line.split(",")
+        #print("Points: ", points)
+        pd.append(int(points[0]))
+        pd.append(int(points[1]))
+
+        x_dist = abs(int(points[0])-int(points[2]))
+        y_dist = abs(int(points[1])-int(points[3]))
+        pd.append(x_dist+y_dist)
+        #print("point_distance: ", pd)
+
+        points_distance.append(pd)
+
+    return points_distance
+
+def check_line(points_distance, row):
+    '''check each of the rows in points,distance 
+    to see if they intersect the row of interest
+    in part 1 test this row is 10
+    in part 1 this row is 2000000
+    I think the amount of coverage is a knight movement based off the
+    off set of the row in interest
+    '''
+    count = 0
+
+    #print("Row of interest: %d"%(row))
+
+    row_covered = {}
+
+    for my_col, my_row, my_dist in points_distance:
+        min_row = my_row - my_dist
+        max_row = my_row + my_dist
+        if row >= min_row and row <= max_row:
+            #print("Col: %d, Row: %d, Dist: %d" %(my_col,my_row,my_dist) )
+            row_dist = my_dist
+            col_dist = 0
+            if row >= my_row:
+                #print("My Row higher than row of interested")
+                while my_row+row_dist >= row:
+                    row_covered[my_col-col_dist] = 1
+                    row_covered[my_col+col_dist] = 1
+                    row_dist -= 1
+                    col_dist += 1
+            if row <= my_row:
+                #print("My Row lower than row of interested")
+                while my_row-row_dist <= row:
+                    row_covered[my_col-col_dist] = 1
+                    row_covered[my_col+col_dist] = 1
+                    row_dist -= 1
+                    col_dist += 1
+    count = len(row_covered.keys())
+    #print("Row covered: %d"%(count))
+
+    return count
+
+def find_row_bounds(points):
+    '''interate through the points and find the min/max'''
+    '''I could shortenthis to just the points overlapping 
+    the row of interest'''
+    #XXX Need to find the width of the row
+    #start is min(x-dist) for each point_distance
+    #end is max(x+dist) for each point_distance
+    row_max = -float('inf')
+    row_min = float('inf')
+
+    for point in points:
+        row_max = max(row_max,point[1]+point[2])
+        row_min = min(row_min,point[1]-point[2])
+
+    return row_min, row_max
+
+def main():
+    """Script entry point"""
+    #print("Hello World")
+    answer = 0
+
+    points_distance = []
+
+    try:
+        file_input = open(sys.argv[1], "r")
+    except IndexError as i_exception:
+        print("No file given: ", i_exception)
+        sys.exit()
+    except FileNotFoundError as f_exception:
+        print("File not found: ", f_exception)
+        sys.exit()
+    
+    lines = file_input.read().splitlines()
+
+    lines = strip_lines(lines)
+
+    #for line in lines:
+    #    print("Line: %s" %(line) )
+
+    points_distance = build_points_distance(lines)
+
+    #for points in points_distance:
+    #    print("point_distance: ", points)
+
+    (row_min,row_max) = find_row_bounds(points_distance)
+
+    #print("Min: %d" %(row_min))
+    #print("Max: %d" %(row_max))
+
+    answer = check_line(points_distance, int(sys.argv[2]))
+
+    print("Answer %d" % (answer) )
+
+if __name__ == "__main__":
+    main()
